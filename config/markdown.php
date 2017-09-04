4652379d3c4603bd601f23695112d4ca08952773
yansongda <me@yansongda.cn>
2017-09-04 22:45:15 +0800

    update config

--- config/markdown.php
+++ config/markdown.php
@@ -11,4 +11,28 @@ return [
         // enables automatic linking of URLs
         'urlsLinked' => true,
     ],
+
+    'parsehtml' => [
+        // strip HTML tags that don't have a Markdown equivalent while preserving the content inside them
+        // '<span>Turnips!</span>';  => $markdown now contains "Turnips!"
+        'strip_tags' => true,
+
+        // strip tags and their content
+        // '<span>Turnips!</span><div>Monkeys!</div>';  => $markdown now contains ""
+        'remove_nodes' => 'span div',
+
+        // 'italic_style', '_'
+        'italic_style' => '_',
+
+        // 'bold_style', '__'
+        // '<em>Italic</em> and a <strong>bold</strong>';  => $markdown now contains "_Italic_ and a __bold__"
+        'bold_style' => '__',
+
+        // 'hard_break', true
+        // TRUE: '<p>test<br>line break</p>'; => $markdown now contains "test\nline break"
+        // FALSE: '<p>test<br>line break</p>'; => $markdown now contains "test  \nline break"
+        'hard_break' => false,
+
+        
+    ],
 ];
